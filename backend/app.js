7854e701550f84c33e75e95d0f24714a1147474e
Anastasiya Fitisova <Anastasiya.Fitisova@yandex.ru>
2022-10-06 19:59:35 +0300

    исправить ошибки бека

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&=]*)/),
+    avatar: Joi.string().regex(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
